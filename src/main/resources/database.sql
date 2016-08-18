62b90645aeabc80b0378d2d0a3ebd3fad8563287
zyuiop <zyuiop@zyuiop.net>
2016-08-18 16:12:16 +0200

    SQL

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS `crosspermissions_entities` (
   PRIMARY KEY (`entity_id`),
   UNIQUE KEY `crosspermissions_entities_user_uuid_uindex` (`entity_uuid`),
   UNIQUE KEY `crosspermissions_entities_entity_name_uindex` (`entity_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS `crosspermissions_options` (
   `option_value` varchar(250) DEFAULT NULL,
   UNIQUE KEY `crosspermissions_options_entity_id_option_name_uindex` (`entity_id`,`option_name`),
   CONSTRAINT `crosspermissions_options_crosspermissions_entities_entity_id_fk` FOREIGN KEY (`entity_id`) REFERENCES `crosspermissions_entities` (`entity_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS `crosspermissions_parents` (
   `parent_id` int(11) DEFAULT NULL,
   UNIQUE KEY `crosspermissions_parents_entity_id_parent_id_uindex` (`entity_id`,`parent_id`),
   CONSTRAINT `crosspermissions_parents_crosspermissions_entities_entity_id_fk` FOREIGN KEY (`entity_id`) REFERENCES `crosspermissions_entities` (`entity_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -38,5 +38,12 @@ CREATE TABLE IF NOT EXISTS `crosspermissions_permissions` (
   `permission_value` tinyint(1) DEFAULT NULL,
   UNIQUE KEY `crosspermissions_permissions_entity_id_permission_uindex` (`entity_id`,`permission_name`),
   CONSTRAINT `crosspermission_perms_entity_id_fk` FOREIGN KEY (`entity_id`) REFERENCES `crosspermissions_entities` (`entity_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE IF NOT EXISTS crosspermissions_uuidcache(
+  player_uuid VARCHAR(50),
+  player_name VARCHAR(50),
+  last_update DATETIME DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE UNIQUE INDEX crosspermissions_uuidcache_player_name_uindex ON crosspermissions_uuidcache (player_name);
+CREATE UNIQUE INDEX crosspermissions_uuidcache_player_uuid_uindex ON crosspermissions_uuidcache (player_uuid);
